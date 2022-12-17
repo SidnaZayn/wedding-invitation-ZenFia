2d6553d455a83608ac5456e68b64bfecb9febb42
zenimaniprima <sidna.zen@imaniprima.com>
2022-12-17 22:10:05 +0700

    add landing page'

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <section id="to">
+      <div data-aos="zoom-in" class="p-4 rounded-3 shadow-sm m-3 text-center" style="background-color:#f8f8f899;">
+        <h1 class="font-2 mb-5" style="color: #65741f;">Wedding Invitation</h1>
+        <div class="img">
+          <img data-aos="zoom-in" data-aos-delay="300" src="/Asset 1-1.png" alt="logo" class="img-to">
+        </div>
+        <h5 data-aos="zoom-in" data-aos-delay="500" style="color: #1b1b1b;" class="mt-5"><i>Dear,</i></h5>
+        <h3 style="text-transform:uppercase;" data-aos="zoom-in" data-aos-delay="600" >{{ $route.params.name }}</h3>
+        <button class="btn btn-outline-secondary mt-4" data-aos="zoom-in" data-aos-delay="700"><RouterLink style="text-decoration: none; color:#1f1f1f;" to="/">Buka Undangan</RouterLink>
+        </button>
+      </div>
+    </section>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup>
+
+</script>
