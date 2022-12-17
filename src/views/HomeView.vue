174fe060d010d364fc7a6215962545156c8be0f4
zenimaniprima <sidna.zen@imaniprima.com>
2022-12-17 20:52:05 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,349 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <section id="home">
+    <div class="containers abs">
+      <div class="text-center">
+        <div data-aos="fade-down" data-aos-duration="1000">
+          <div class="p-4 rounded-3 mt-2 mx-3 shadow" style="background-color:#f8f8f8d7;">
+            <h5 class="card-title cal fs-2">The Wedding of</h5>
+            <h1 class="font-2">Sidna & Ariesty</h1>
+            <small class="card-text">Kepada Bapak/Ibu/Saudara/i Yang Terhormat</small>
+            <p class="card-text">Izinkan kami mengundang Anda dalam acara kami</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="img">
+      <img src="we.jpg" class="card-img img1">
+    </div>
+  </section>
+  <section id="dalil">
+    <div class="text-center my-5 dalil-container">
+      <div class="img">
+        <img data-aos="zoom-in" data-aos-delay="300" src="flower-1.png" alt="flower" class="abs img3">
+      </div>
+      <div class="img">
+        <img data-aos="zoom-in" src="flower-1.png" alt="flower" class="abs img4">
+      </div>
+      <div class="p-4 rounded-3 mx-3 shadow-sm" style="background-color:#f8f8f899;">
+        <h2 class="card-title font-2" data-aos="flip-right">Ar-Rum : 21</h2>
+        <p class="mt-3 arabic" data-aos="flip-right">وَمِنْ اٰيٰتِهٖٓ اَنْ خَلَقَ لَكُمْ مِّنْ اَنْفُسِكُمْ
+          اَزْوَاجًا
+          لِّتَسْكُنُوْٓا
+          اِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَّوَدَّةً وَّرَحْمَةً ۗاِنَّ فِيْ ذٰلِكَ لَاٰيٰتٍ لِّقَوْمٍ
+          يَّتَفَكَّرُوْنَ
+        </p>
+        <p data-aos="flip-right">"Dan di antara tanda-tanda kekuasaan-Nya ialah Dia
+          menciptakan untukmu isteri-isteri dari jenismu sendiri,
+          supaya kamu cenderung dan merasa tenteram kepadanya,
+          dan dijadikan-nya diantaramu rasa kasih dan sayang.
+          Sesungguhnya pada yang demikian itu benar-benar
+          terdapat tanda-tanda bagi kaum yang berfikir"</p>
+      </div>
+    </div>
+  </section>
+  <section id="we">
+    <div class="container">
+      <div class="card mb-3 mx-5" data-aos="flip-right" data-aos-duration="1500">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="sidna.jpg" class="img-fluid rounded" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body text-center p-0 mt-4 mb-2">
+              <p>🤴</p>
+              <h1 class="card-title cal">Sidna Muhammad Zen</h1>
+              <small>Putra Dari</small>
+              <h5 class="card-text font-2">Bpk. Akhmad Faozi & Ibu. Heriyanti</h5>
+              <p class="card-text">Jl. Mawardi RT.02, RW.01 desa Adiwerna, Kec. Adiwerna,Kab.Tegal</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card mb-3 mx-5" data-aos="flip-right" data-aos-duration="1500">
+        <div class="row g-0">
+          <div class="col-md-8">
+            <div class="card-body text-center p-0 mt-4 mb-2">
+              <p>👸</p>
+              <h1 class="card-title cal">Ariesty Rizky Aliefia</h1>
+              <small>Putri Dari</small>
+              <h5 class="card-text font-2">Bpk. Imam Santoso<span style="font-size: small;">(alm)</span> & Ibu.
+                Widiyanti</h5>
+              <p class="card-text">Jl.Sawo Gang 1 Rt/RW 04/02 Kel.Procot Kec.Slawi Kab.Tegal</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <img src="fia.jpg" class="img-fluid rounded" alt="...">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="schedule">
+    <div class="text-center">
+      <div class="img">
+        <img data-aos="zoom-in" data-aos-delay="300" src="flower-3-copy.png" alt="flower" class="abs img7">
+        <img data-aos="zoom-in" data-aos-delay="300" src="flower-3.png" alt="flower" class="abs img6">
+      </div>
+      <div class="p-4 rounded-3 shadow-sm mt-2 mx-3" style="background-color:#f8f8f899;">
+        <h5 class="card-title font-2" data-aos="zoom-in">Susunan Acara</h5>
+        <div>
+          <h1 class="mt-3 cal" data-aos="fade-right">Akad & Resepsi</h1>
+          <p class="m-0" data-aos="flip-left"><i class="bi bi-calendar4-event"></i> Minggu, 12-03-23</p>
+          <p class="m-0" data-aos="flip-left"><i class="bi bi-clock"></i> 10.00 WIB-Selesai</p>
+          <p class="m-0" data-aos="flip-left"><i class="bi bi-geo-alt"></i> Jl.Sawo Gang 1 Rt/RW 04/02
+            Kel.Procot
+            Kec.Slawi Kab.Tegal
+          </p>
+        </div>
+        <!-- <div>
+                <h1 class="mt-5 cal" data-aos="fade-right">Ngunduh Mantu</h1>
+                <p class="m-0" data-aos="flip-right"><i class="bi bi-calendar4-event"></i> Senin, 13-03-23</p>
+                <p class="m-0" data-aos="flip-right"><i class="bi bi-clock"></i> 10.00 WIB</p>
+                <p class="m-0" data-aos="flip-right"><i class="bi bi-geo-alt"></i> Jl.Sawo Gang 1 Rt/RW 04/02
+                    Kel.Procot
+                    Kec.Slawi Kab.Tegal
+                </p>
+            </div> -->
+      </div>
+    </div>
+  </section>
+  <section id="presensi">
+    <!-- Button trigger modal -->
+    <div class="text-center m-4">
+      <button type="button" data-aos="zoom-in" class="btn btn-outline-secondary" data-bs-toggle="modal"
+        data-bs-target="#exampleModal">
+        <i class="bi bi-file-text"></i> <br> Konfirmasi Kehadiran
+      </button>
+    </div>
+    <section id="countdown">
+      <div class="p-4 rounded-3 shadow-sm m-3 text-center" style="background-color:#f8f8f899;">
+        <h1 class="cal" data-aos="flip-down">Menuju Acara</h1>
+        <hr class="hr-1 mb-4" data-aos="zoom-in">
+        <div class="row">
+          <div class="col" data-aos="flip-right">
+            <div class="p-3 rounded-3 shadow-sm mb-2" style="background-color: #FCE592;">
+              <h1>{{ menuju_halal.days }}</h1>
+            </div>
+            <h5>Hari</h5>
+          </div>
+          <div class="col" data-aos="flip-right" data-aos-delay="200">
+            <div class="p-3 rounded-3 shadow-sm mb-2" style="background-color: #FCE592;">
+              <h1>{{ menuju_halal.hours }}</h1>
+            </div>
+            <h5>Jam</h5>
+          </div>
+          <div class="col" data-aos="flip-right" data-aos-delay="300">
+            <div class="p-3 rounded-3 shadow-sm mb-2" style="background-color: #FCE592;">
+              <h1>{{ menuju_halal.minutes }}</h1>
+            </div>
+            <h5>Menit</h5>
+          </div>
+          <div class="col" data-aos="flip-right" data-aos-delay="400">
+            <div class="p-3 rounded-3 shadow-sm mb-2" style="background-color: #FCE592;">
+              <h1>{{ menuju_halal.seconds }}</h1>
+            </div>
+            <h5>Detik</h5>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">apakah anda akan hadir?</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="mb-3">
+                <input type="text" id="" class="form-control" placeholder="Nama">
+              </div>
+              <div class="mb-3">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                  placeholder="Sampaikan Pesanmu disini !"></textarea>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer text-center">
+            <button type="button" class="btn btn btn-outline-secondary" data-bs-dismiss="modal">😔 Saya Tidak
+              Hadir</button>
+            <button type="button" class="btn btn btn-outline-secondary" data-bs-dismiss="modal">🥳 Saya Hadir</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="amplopDigital">
+    <div class="img text-center p-4" style="background:  url(bg-flower.png); background-size: 50%;">
+      <img data-aos="zoom-in" data-aos-duration="200" src="sa.jpg" class="card-img shadow img2">
+    </div>
+    <div class="text-center">
+      <div class="p-4 rounded-3 mt-2 mx-3 shadow-sm" data-aos="flip-down" style="background-color:#f8f8f899;">
+        <h2 class="card-title cal">Amplop Digital</h2>
+        <small class="card-text mt-3">Doa restu anda merupakan karunia yang sangat berarti bagi kami dan
+          jika
+          memberi adalah ungkapan tanda terima kasih anda, anda dapat memberi kado secara cashless</small>
+      </div>
+      <div class="row mt-2 p-3">
+        <div class="col" data-aos="flip-right">
+          <div class="card shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title cal">Via Bank</h5>
+              <img src="mandiri.jpg" class="card-img-top" alt="...">
+              <p class="card-text">1390026051932 an. <br> Ariesty Rizky Aliefia (Mandiri)</p>
+              <img src="bri.jpg" class="card-img-top" alt="...">
+              <p class="card-text">0545 01016247509 an. <br> Sidna Muhammad Zen (BRI)</p>
+            </div>
+          </div>
+        </div>
+        <div class="col" data-aos="flip-right" data-aos-delay="300">
+          <div class="card shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title cal">Via Dompet Digital</h5>
+              <img src="gopay.jpg" class="card-img-top" alt="...">
+              <p class="card-text">081226158294 an. <br> Sidna Muhammad Zen</p>
+              <img src="ovo.jpg" class="card-img-top" alt="...">
+              <p class="card-text">081226158294 an. <br> Sidna Muhammad Zen</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="Story">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" data-bs-interval="500">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="kita.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="kita1.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="kita2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="kita3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="kita3.jpg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+    </div>
+    <div class="text-center">
+      <div class="img">
+        <img data-aos="zoom-in" data-aos-delay="300" src="flower-2.png" alt="flower" class="abs img6">
+        <img data-aos="zoom-in" data-aos-delay="300" src="flower-2-copy.png" alt="flower" class="abs img7">
+      </div>
+      <div class="p-4 rounded-3 m-4 shadow-sm" style="background-color:#f8f8f899;">
+        <h1 data-aos="fade-down" class="card-title cal">Our Story</h1>
+        <hr class="hr-1">
+        <div data-aos="fade-right">
+          <h3 class="mt-5 mb-0 cal">First Impresion</h3>
+          <p class="m-0">22-02-22</p>
+          <p class="card-text">Kisah ini berawal dari pertemuan kami sebagai kawan kerja di sebuah
+            restoran
+            kecil di jantung Kab. Tegal. Canda, tawa, berbagi kisah menjadi pendekat kami diawal
+            perjumpaan.
+          </p>
+        </div>
+        <div data-aos="fade-left">
+          <h3 class="mt-5 mb-0 cal">Engagement</h3>
+          <p class="m-0">11-12-22</p>
+          <p class="card-text">Perkenalan kami membawa pada keseriusan hubungan hingga kami sepakat untuk
+            melangsungkan acara Khitbah pada Minggu, 12 Desember 2022
+          </p>
+        </div>
+        <div data-aos="fade-right">
+          <h3 class="mt-5 mb-0 cal">Finally</h3>
+          <p class="m-0">12-03-23</p>
+          <p class="card-text">12 Maret 2022, akan menjadi sejarah dalam kehidupan kami berdua sebagai
+            titik
+            dimulainya kehidupan baru. Doakan kami agar dapat mengarungi bahtera rumah tangga hingga
+            sampai
+            di</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="footer">
+
+    <div class="img p-3 text-center" data-aos="zoom-in">
+      <img src="akhir.jpg" class="card-img img5 shadow">
+    </div>
+    <div class="text-center mt-2 mb-4 mx-3" data-aos="fade-up" style="height:7vh;">
+      <p class="card-text"> Kesan yang mendalam akan terukir dihati kami,
+        serta diiringi ucapan terima kasih yang tulus,
+        kepada Bapak/Ibu/Saudara/i berkenan hadir
+        untuk memberikan Doa Restu
+      </p>
+      <hr class="hr-2">
+      <h1 class="cal">Terima Kasih</h1>
+    </div>
+    <div class="img text-center mt-5">
+      <img src="flower-4.png" class="card-img img8 mt-3">
+    </div>
+  </section>
+
+</template>
+
+<script setup>
+import { reactive,onMounted } from "vue";
+
+onMounted(()=>{
+  myAudioFunction();
+});
+
+function myAudioFunction() {
+  var x = document.createElement("AUDIO");
+
+  x.setAttribute("src",  "songs.mp3");
+  x.setAttribute("autoplay", "");
+  document.body.appendChild(x);
+};
+
+let menuju_halal = reactive({
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+});
+// Set the date we're counting down to
+const countDownDate = new Date("Mar 12, 2023 09:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  menuju_halal.seconds = seconds;
+  menuju_halal.minutes = minutes;
+  menuju_halal.hours = hours;
+  menuju_halal.days = days;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
