a2f4319fdc9b889ad80c581ac303e62681bc516a
zettravos321 <sidna.zen@imaniprima.com>
2023-01-31 15:12:40 +0700

    :passport_control: login page
    login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFoundPageView from '../views/NotFoundView.vue'
+import Dashboard from "../views/Dashboard.vue";
+import Login from "../views/Login.vue";
+
+const routes = [
+  {
+    path: '/to/:name',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    redirect: '/to/Fulan',
+    component: NotFoundPageView
+  },
+  {
+    path:"/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
+    meta:{
+      isAuth: true
+    }
+  },
+  {
+    path:"/login",
+    name: "Login",
+    component: Login,
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/to/:name',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      redirect: '/to/Fulan',
-      component: NotFoundPageView
-    },
-  ]
-})
+  routes,
+  linkActiveClass: "active",
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(store.getters.isAuthenticated)
+  if (to.matched.some((record) => record.meta.isAuth)) {
+    if (localStorage.getItem('login')) {
+      next();
+      return;
+    }
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="container">
+      <div
+        class="
+          d-flex
+          flex-column
+          min-vh-100
+          justify-content-center
+          align-items-center
+        "
+      >
+        <div class="card shadow mx-auto p-4">
+          <h2 class="mx-auto">Login</h2>
+          <div class="form">
+            <div class="my-4">
+              <label for="username" class="mb-2">username</label>
+              <input
+                class="form-control"
+                type="text"
+                v-model="login.username"
+              />
+            </div>
+            <div class="my-4">
+              <label for="password" class="mb-2">password</label>
+              <input
+                class="form-control"
+                type="password"
+                v-model="login.password"
+              />
+            </div>
+            <button class="btn btn-success mt-3" @click="loginDashboard()">
+              Masuk
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import { useRouter } from "vue-router";
+
+const login = ref({
+  username: "",
+  password: "",
+});
+const router = useRouter();
+
+const loginDashboard = () => {
+  if (login.value.username == "fia") {
+    if (login.value.password == "procot321") {
+      localStorage.setItem("login", "procot321");
+      alert("berhasil masuk");
+      router.push({ name: "Dashboard" });
+    } else {
+      alert("password salah");
+    }
+  } else {
+    alert("username salah");
+  }
+};
+
+onMounted(() => {
+  localStorage.removeItem("login");
+});
+</script>
